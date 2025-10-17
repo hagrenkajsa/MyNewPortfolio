d88d1beceb982c3df44d1b9721049bfa726148f5
id20khn <id20khn@cs.umu.se>
2025-10-17 10:23:58 +0200

    Initial commit

---
+++ animation.js
@@ -0,0 +1,8 @@
+//======Toggle-menue=====
+const menu = document.querySelector('.menu');
+const menuTogle = document.querySelector('.menu-toggle');
+
+menuTogle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuTogle.classList.toggle('open');
+});
